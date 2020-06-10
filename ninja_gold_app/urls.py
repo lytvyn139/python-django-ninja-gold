eabee57f8b14bb6dc1705ce3187d28f86e2d67e5
IURII LYTVYN <lytvyn.139@gmail.com>
2020-06-10 18:43:00 -0500

    all logic is working

--- ninja_gold_app/urls.py
+++ ninja_gold_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('reset', views.reset),
+    path('reset_all', views.reset_all),
+    path('reset_scores', views.reset_scores),
     path('process_money', views.process)
 ]
